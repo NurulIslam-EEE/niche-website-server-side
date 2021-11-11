6af031048a39f58c74ff71195c4089b4d7b61206
NurulIslam-EEE <ni163034@gmail.com>
2021-11-11 20:51:07 +0600

    orderCollection added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 const port = process.env.PORT || 5000
 
 app.use(cors());
@@ -16,8 +17,81 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        const database = client.db('doctors_portal');
-        const appointmentsCollection = database.collection('appointments');
+        const database = client.db('imperial-cars');
+        const carsCollection = database.collection('cars');
+        const usersCollection = database.collection('users');
+        const ordersCollection = database.collection('orders');
+
+        app.post('/addProduct', async (req, res) => {
+            const user = req.body;
+            const result = await carsCollection.insertOne(user)
+            res.json(result)
+            console.log(result);
+        })
+
+        //order 
+        app.post('/booking', async (req, res) => {
+            const user = req.body;
+            const result = await ordersCollection.insertOne(user)
+            res.json(result)
+            console.log(result);
+        })
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user)
+            res.json(result)
+            console.log(user);
+        })
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const option = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await usersCollection.updateOne(filter, updateDoc, option);
+            res.json(result)
+            console.log(result);
+        })
+
+        //find admin 
+        app.put('/users/makeAdmin', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const updateDoc = { $set: { role: "admin" } };
+            // const option = { upsert: true };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.json(result)
+            console.log(result)
+        })
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const result = await usersCollection.findOne(filter);
+            console.log(result);
+            res.json(result)
+        })
+        //get user orders
+        app.get('/myOrders/:email', async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const result = await ordersCollection.find(filter).toArray();
+            console.log(result);
+            console.log(email);
+            res.json(result)
+        })
+        //get all products
+        app.get('/products', async (req, res) => {
+            const result = await carsCollection.find({}).toArray();
+            console.log(result);
+            res.json(result)
+        })
+        //booking 
+        app.get('/booking/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const result = await carsCollection.findOne(filter);
+            console.log(result);
+            res.json(result)
+        })
     }
     finally {
         // await client.close(); 
